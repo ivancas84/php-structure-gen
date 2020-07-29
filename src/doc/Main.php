b321ad2b9ae4c8f1bba3b500f96cfe7383b4d3dc
ivancas84 <ivancas84@gmail.com>
2020-07-29 10:06:40 -0300

    Modificar constantes de rutas

--- src/doc/Main.php
+++ src/doc/Main.php
@@ -7,7 +7,7 @@ require_once("GenerateFileEntity.php");
 class Doc extends GenerateFileEntity {
 
   public function __construct(Entity $entity) {
-    $directorio = $_SERVER["DOCUMENT_ROOT"]."/".PATH_DOC."/relations/";
+    $directorio = $_SERVER["DOCUMENT_ROOT"]."/".PATH_ROOT."/doc/relations/";
     $nombreArchivo = $entity->getName() . ".txt";
     parent::__construct($directorio, $nombreArchivo, $entity);
   }
