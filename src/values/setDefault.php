e8a61f9bd85ec541f487fb0b9f5fe1ec657d0757
ivancas84 <ivancas84@gmail.com>
2020-08-01 16:53:30 -0300

    Modificar metodo setDefault, los valores por defecto se definen directamente en este metodo y no en los setters

--- src/values/setDefault.php
+++ src/values/setDefault.php
@@ -19,9 +19,14 @@ class GenValues_setDefault extends GenerateEntity {
   public function body(){
     $pkNfFk = $this->getEntity()->getFields();
 
-    foreach ( $pkNfFk as $field ) {      
-        $this->string .= "    \$this->set{$field->getName('XxYy')}(DEFAULT_VALUE);
-";
+    foreach ( $pkNfFk as $field ) {
+      switch($field->getDataType()){
+        case "date": case "timestamp": case "year": case "time": $this->datetime($field); break; 
+        case "integer": $this->withoutQuotes($field); break;
+        default: $this->withQuotes($field); break;
+      }
+      
+      
     }
   }
 
@@ -32,6 +37,31 @@ class GenValues_setDefault extends GenerateEntity {
 ";
   }
 
+
+  protected function withoutQuotes($field){
+    $default = ($field->getDefault()) ? $field->getDefault() : "null";
+    $this->string .= "    \$this->set{$field->getName('XxYy')}({$default});
+";
+  }
+  
+  protected function withQuotes($field){
+    $default = ($field->getDefault()) ? "'".$field->getDefault() . "'": "null";
+    $this->string .= "    \$this->set{$field->getName('XxYy')}({$default});
+";
+  }
+
+  protected function datetime($field){
+    if(strpos(strtolower($field->getDefault()), "current") !== false){
+      $default = "date('c')";
+    } else {
+      $default = ($field->getDefault()) ? "'" . $field->getDefault() . "'" : "null";
+    }
+    $this->string .= "    \$this->set{$field->getName('XxYy')}({$default});
+";
+  }
+
+
+  
 
 
 }
