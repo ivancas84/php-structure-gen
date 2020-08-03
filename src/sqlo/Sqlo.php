de3f8ae564a38347e4eb7a3d31434f139a958e50
ivancas84 <ivancas84@gmail.com>
2020-08-03 09:06:43 -0300

    Incorporar propiedad entityName y enviar constructor al padre

--- src/sqlo/Sqlo.php
+++ src/sqlo/Sqlo.php
@@ -38,14 +38,7 @@ class _" . $this->getEntity()->getName("XxYy") . "Sqlo extends EntitySqlo {
 
   protected function construct(){
     $this->string .= "
-  public function __construct(){
-    /**
-     * Se definen todos los recursos de forma independiente, sin parametros en el constructor, para facilitar el polimorfismo de las subclases
-     */
-    \$this->db = Dba::dbInstance();
-    \$this->entity = Entity::getInstanceRequire('{$this->getEntity()->getName()}');
-    \$this->sql = EntitySql::getInstanceRequire('{$this->getEntity()->getName()}');
-  }
+  public \$entityName = \"{$this->getEntity()->getName()}\";
 
 ";
   }
