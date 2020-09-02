9e57d7bd11ebd04a8904f47dbf60c6b0cc7cba09
ivancas84 <ivancas84@gmail.com>
2020-09-02 09:53:29 -0300

    Eliminar generacion de apis sin codigo

--- src/index.php
+++ src/index.php
@@ -9,14 +9,7 @@ require_once("class/tools/Filter.php");
 $generate = Filter::get("gen");
 
 foreach($structure as $entity) {
-    switch($generate){
-        //apis
-        case "api_all": apiAll($entity); break;
-		case "api_count": apiCount($entity); break;
-		case "api_get_all": apiGetAll($entity); break;
-		case "api_ids": apiIds($entity); break;
-		case "api_unique": apiUnique($entity); break;
-
+  switch($generate){
 		//doc
 		case "doc": doc($entity); break;
 		
@@ -25,8 +18,8 @@ foreach($structure as $entity) {
 		case "sql": sql($entity); break;
 		case "values": values($entity); break;
         
-        default:
-        	apiAll($entity);
+    default:
+      apiAll($entity);
 			apiCount($entity);
 			apiGetAll($entity);			
 			apiIds($entity);			
@@ -38,60 +31,60 @@ foreach($structure as $entity) {
 			sql($entity);
 			values($entity);
 		break;
-    }
+  }
 }
 
 function apiAll(Entity $entity){
-    require_once("api/All.php");
-    $gen = new GenControllerAll($entity);
-    $gen->generateIfNotExistsWithImp();
+  require_once("api/All.php");
+  $gen = new GenControllerAll($entity);
+  $gen->generateIfNotExistsWithImp();
 }
 
 function apiCount(Entity $entity){
-    require_once("api/Count.php");
-    $gen = new GenControllerCount($entity);
-    $gen->generateIfNotExistsWithImp();
+  require_once("api/Count.php");
+  $gen = new GenControllerCount($entity);
+  $gen->generateIfNotExistsWithImp();
 }
 
 function apiGetAll(Entity $entity){
-    require_once("api/GetAll.php");
-    $gen = new GenControllerGetAll($entity);
-    $gen->generateIfNotExistsWithImp();
+  require_once("api/GetAll.php");
+  $gen = new GenControllerGetAll($entity);
+  $gen->generateIfNotExistsWithImp();
 }
 
 function apiIds(Entity $entity){
-    require_once("api/Ids.php");
-    $gen = new GenControllerIds($entity);
-    $gen->generateIfNotExistsWithImp();
+  require_once("api/Ids.php");
+  $gen = new GenControllerIds($entity);
+  $gen->generateIfNotExistsWithImp();
 }
 
 function apiUnique(Entity $entity){
-    require_once("api/Unique.php");
-    $gen = new GenControllerUnique($entity);
-    $gen->generateIfNotExistsWithImp();
+  require_once("api/Unique.php");
+  $gen = new GenControllerUnique($entity);
+  $gen->generateIfNotExistsWithImp();
 }
 
 
 function doc(Entity $entity){
-    require_once("doc/Main.php");
-    $gen = new Doc($entity);
-    $gen->generate();
+  require_once("doc/Main.php");
+  $gen = new Doc($entity);
+  $gen->generate();
 }
 
 function sqlo(Entity $entity){
-    require_once("sqlo/Sqlo.php");
-    $gen = new GenClassSqlo($entity);
-    $gen->generate();
-  }
+  require_once("sqlo/Sqlo.php");
+  $gen = new GenClassSqlo($entity);
+  $gen->generate();
+}
 
 function sql(Entity $entity){
-    require_once("sql/Sql.php");
-    $gen = new GenClassSql($entity);
-    $gen->generate();
+  require_once("sql/Sql.php");
+  $gen = new GenClassSql($entity);
+  $gen->generate();
 }
 
 function values(Entity $entity){
-    require_once("values/Values.php");
-    $gen = new GenClassValues($entity);
-    $gen->generate();
+  require_once("values/Values.php");
+  $gen = new GenClassValues($entity);
+  $gen->generate();
 }
