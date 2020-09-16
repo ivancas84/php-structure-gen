11de69d0c16d6f40dbc218996fe2fed810740671
ivancas84 <ivancas84@gmail.com>
2020-09-16 10:10:42 -0300

    Quitar chequeo de longitud para los date

--- src/value/_validators.php
+++ src/value/_validators.php
@@ -11,7 +11,7 @@ class GenValue_validators extends GenerateEntity {
     foreach ( $pkNfFk as $field ) {
 
       switch($field->getDataType()){
-        case "date": case "timestamp": case "year": case "time": $this->checkMethod($field, ["isA('DateTime')"]); break;
+        case "date": case "timestamp": case "year": case "time": $this->date($field); break;
         default: $this->checkMethod($field);
       }
     }
@@ -28,22 +28,35 @@ class GenValue_validators extends GenerateEntity {
 ";
   }
 
+  protected function _check($field, $methods){
+    $this->string .= "  public function check{$field->getName('XxYy')}() { 
+      \$this->_logs->resetLogs(\"{$field->getName()}\");
+      if(Validation::is_undefined(\$this->{$field->getName('xxYy')})) return null;
+      \$v = Validation::getInstanceValue(\$this->{$field->getName('xxYy')})->" . implode("->", $methods). ";
+      foreach(\$v->getErrors() as \$error){ \$this->_logs->addLog(\"{$field->getName()}\", \"error\", \$error); }
+      return \$v->isSuccess();
+    }
+  
+  ";
+  }
+
+  protected function date($field){
+    /**
+     * Similar a checkMethod pero se omiten las longitudes
+     */
+    $methods = ["isA('DateTime')"];
+    if ($field->isNotNull()) array_unshift($methods, "required()"); //required debe chequearse primero
+    if(empty($methods)) return $this->success($field);
+    $this->_check($field, $methods);
+    
+  }
   
   protected function checkMethod($field, array $methods = []){
     if ($field->isNotNull()) array_unshift($methods, "required()"); //required debe chequearse primero
     if ($l = $field->getLength()) array_push($methods, "max({$l})");
     if ($l = $field->getMinLength()) array_push($methods, "min({$l})");
     if(empty($methods)) return $this->success($field);
-
-    $this->string .= "  public function check{$field->getName('XxYy')}() { 
-    \$this->_logs->resetLogs(\"{$field->getName()}\");
-    if(Validation::is_undefined(\$this->{$field->getName('xxYy')})) return null;
-    \$v = Validation::getInstanceValue(\$this->{$field->getName('xxYy')})->" . implode("->", $methods). ";
-    foreach(\$v->getErrors() as \$error){ \$this->_logs->addLog(\"{$field->getName()}\", \"error\", \$error); }
-    return \$v->isSuccess();
-  }
-
-";
+    $this->_check($field, $methods);
   }
 
 }
