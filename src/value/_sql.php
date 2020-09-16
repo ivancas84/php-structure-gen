3deb3a91d51b5f748c3380245460122bdd6a41cc
ivancas84 <ivancas84@gmail.com>
2020-09-16 08:17:04 -0300

    Incorporar metodos necesarios para el funcionamiento de las condiciones

--- src/value/_sql.php
+++ src/value/_sql.php
@@ -5,17 +5,31 @@ class GenValue_sql extends GenerateEntity {
 
 
    public function generate(){
-     $pkNfFk = $this->getEntity()->getFieldsByType(["pk", "nf", "fk"]);
+    $pkNfFk = $this->getEntity()->getFieldsByType(["pk", "nf", "fk"]);
 
-     foreach ( $pkNfFk as $field ) {
-       switch($field->getDataType()){
-         case "date": $this->dateTime($field, "Y-m-d"); break;
-         case "time": $this->dateTime($field, "H:i:s"); break;
-         case "year": $this->dateTime($field, "Y"); break;
-         case "timestamp": $this->dateTime($field, "Y-m-d H:i:s"); break;
-         case "boolean": $this->boolean($field); break;
-         case "integer": case "float": $this->number($field); break;
-         case "string": case "text": $this->text($field); break;
+    foreach ( $pkNfFk as $field ) {
+      switch($field->getDataType()){
+        case "date": 
+          $this->dateTime($field, "Y-m-d");
+          $this->dateTime($field, "Y-m", "Ym");
+          $this->dateTime($field, "Y", "Y");
+        break;
+        case "time": 
+          $this->dateTime($field, "H:i:s");
+          $this->dateTime($field, "H:i", "Hm"); 
+        break;
+        case "year": 
+           $this->dateTime($field, "Y"); 
+        break;
+        case "timestamp": 
+          $this->dateTime($field, "Y-m-d H:i:s");
+          $this->dateTime($field, "Y-m-d", "Date");
+          $this->dateTime($field, "Y-m", "Ym");
+          $this->dateTime($field, "Y", "Y"); 
+        break;
+        case "boolean": $this->boolean($field); break;
+        case "integer": case "float": $this->number($field); break;
+        case "string": case "text": $this->text($field); break;
        }
      }
      return $this->string . "
@@ -24,8 +38,8 @@ class GenValue_sql extends GenerateEntity {
     }
 
 
-  protected function dateTime(Field $field, $format){
-    $this->string .= "  public function sql{$field->getName('XxYy')}() { return \$this->_sqlDateTime(\$this->{$field->getName('xxYy')}, \"{$format}\"); }
+  protected function dateTime(Field $field, $format, $sufix = ""){
+    $this->string .= "  public function sql{$field->getName('XxYy')}{$sufix}() { return \$this->_sqlDateTime(\$this->{$field->getName('xxYy')}, \"{$format}\"); }
 ";
   }
 
